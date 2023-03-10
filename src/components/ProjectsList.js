f6ad71eecb132185c26229ed9f5ba6102374d18e
Kojo Obeng-Arhin <kwadwoarhin@hotmail.com>
2023-03-09 23:47:40 -0500

    Changed link for spotify

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -39,42 +39,53 @@ const ProjectsList = () => {
         }
       )
       .then((response) => {
-        return response.json()
+        return response.json();
       })
       .then((json) => {
-        setProjects(json.data.projectsCollection.items)
+        setProjects(json.data.projectsCollection.items);
       });
   }, [query, accessToken, spaceId]);
 
   if (!projects) return null;
 
   return (
-    <div className="p-14">
-      <div className="flex mb-5">
-        <AnimatedHeader word={"Things"} />
-        <span className="w-3" />
-        <AnimatedHeader word={"I've"} />
-        <span className="w-3" />
-        <AnimatedHeader word={"Built"} />
+    <a href={project.clickLink} target="_blank">
+      <div className="p-14">
+        <div className="flex mb-5">
+          <AnimatedHeader word={"Things"} />
+          <span className="w-3" />
+          <AnimatedHeader word={"I've"} />
+          <span className="w-3" />
+          <AnimatedHeader word={"Built"} />
+        </div>
+        <p className="font-source text-white lg:text-lg md:text-lg sm:text-md text-sm mb-5">
+          Learn more by clicking on the Github link!
+        </p>
+        <div className="grid 2xl:grid-cols-6 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          {projects.map((project, index) => {
+            const {
+              name,
+              description,
+              techStack,
+              githubLink,
+              devpostLink,
+              projectImageCollection,
+            } = project;
+            return (
+              <ProjectCard
+                name={name}
+                description={description}
+                techStack={techStack}
+                githubLink={githubLink}
+                devpostLink={devpostLink}
+                imageURL={projectImageCollection.items[0].url}
+                index={index}
+              />
+            );
+          })}
+        </div>
       </div>
-      <p className="font-source text-white lg:text-lg md:text-lg sm:text-md text-sm mb-5">
-        Learn more by clicking on the Github link!
-      </p>
-      <div className="grid 2xl:grid-cols-6 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-        {projects.map((project, index) => {
-          const {name, description, techStack, githubLink, devpostLink, projectImageCollection} = project
-          return (<ProjectCard
-            name={name}
-            description={description}
-            techStack={techStack}
-            githubLink={githubLink}
-            devpostLink={devpostLink}
-            imageURL={projectImageCollection.items[0].url}
-            index={index}
-          />)
-        })}
-      </div>
-    </div>
+    </a>
   );
 };
 
